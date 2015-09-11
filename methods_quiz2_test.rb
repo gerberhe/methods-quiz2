66b906dca7b97f6ffcb9001c9581a6a98a11bd2a
Harrison Gerber <gerberhe@s.dcsdk12.org>
2015-09-11 10:00:57 -0600

    Created tests for method max_maybe

--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -19,4 +19,11 @@ class MethodsQuizTest < MiniTest::Test
 		assert_equal 6,@m.without_doubles(1,5)
 		assert_equal 2,@m.without_doubles(6,6)
 	end
+
+	def test_max_maybe
+		assert_equal 7,@m.max_maybe(7,2)
+		assert_equal 0,@m.max_maybe(4,4)
+		assert_equal 5,@m.max_maybe(5,10)
+		assert_equal 6,@m.max_maybe(6,11)
+	end
 end
