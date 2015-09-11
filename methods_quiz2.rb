5a2a653873ba78917cfbcec5918e22cdc13dff58
Harrison Gerber <gerberhe@s.dcsdk12.org>
2015-09-11 09:58:42 -0600

    Method without_doubles complete

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,18 @@
-module MethodsQuiz
+module MethodsQuiz2
 	
-	
+	def without_doubles(a,b)
+		if numbers_equal?(a,b) == true
+			a == 6 ? (a = 1) && (b = 1) : a = (a + 1)
+		end
+
+		return a + b
+	end
+
+
+	private
+
+		def numbers_equal?(a,b)
+			a == b
+		end
 
 end
