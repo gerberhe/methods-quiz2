673e5928db2bf8eb43774b1a6fb9bf9ef481f5c5
Harrison Gerber <gerberhe@s.dcsdk12.org>
2015-09-11 09:36:56 -0600

    Added tests for method without_doubles

--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -13,4 +13,10 @@ class MethodsQuizTest < MiniTest::Test
 
 	# TODO - write tests here
 
+	def test_without_doubles
+		assert_equal 7,@m.without_doubles(3,4)
+		assert_equal 9,@m.without_doubles(4,4)
+		assert_equal 6,@m.without_doubles(1,5)
+		assert_equal 2,@m.without_doubles(6,6)
+	end
 end
